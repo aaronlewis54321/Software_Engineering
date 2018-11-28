e1aead65e4d23bfe4df747c884d2e38c099ac2df
mrinoldo <31969521+mrinoldo@users.noreply.github.com>
2018-11-28 14:44:58 -0500

    Revert "Revert "Merge branch 'dateTimePicker'""
    This reverts commit d6331289a4cc9fc8b2a48dcb80402000bbc9bee4.

--- AdminstrationApp/src/AdminstrationApp/Controller.java
+++ AdminstrationApp/src/AdminstrationApp/Controller.java
@@ -5,6 +5,7 @@
  */
 package AdminstrationApp;
 
+import AdminstrationApp.DateTimePicker;
 import java.io.IOException;
 import static javafx.application.Application.launch;
 
@@ -13,37 +14,13 @@ import static javafx.application.Application.launch;
  * @author alewis91
  */
 public class Controller {
-    
+
     Model m;
-    
-    
-    public Controller(Model model) throws IOException
-    {
+
+    public Controller(Model model) throws IOException {
         m = model;
     }
-    
-    
-    
-    //Funcitonality needs to be implemented
-    public void btnAddUsersAction(){
-        System.out.println("AddUsers");
-    }
-    
-    public void btnEditGroupsAction(){
-        System.out.println("EditGroups");
-    }
-    
-    
-    //Functionality needs to be implemented
-    public void btnApplyChangesAction() {
-        System.out.println("ApplyChanges");
-    }
-    
-    
-    //Functionality needs to be implemented
-    public void btnRevertChangesAction() {
-        System.out.println("RevertChanges");
-    }
+
     
     public void btnExportDataAction() throws IOException {
         m.writeResponseToCSV();
@@ -51,11 +28,11 @@ public class Controller {
 
     void btnSubmitAction(View v) throws IOException {
         v.determineScheduledUsers();
-        m.scheduleUsers(v.getScheduleTime(),v.getScheduledUsers());
-        m.makeUsersInactive(v.getScheduledUsers(), v.getScheduleTime());
+        m.scheduleUsers(v.getSchedule(), v.getScheduledUsers());
+        m.makeUsersInactive(v.getScheduledUsers(), v.getSchedule());
         v.clearScheduledUsers();
         btnRefreshAction(v);
-        
+
         //System.out.println(v.table.getSelectionModel().getSelectedItems());
     }
 
@@ -63,9 +40,9 @@ public class Controller {
         v.refreshTable();
     }
 
-    void btnHelpAction() {
-        
+    void btnReactivateAllAction(View v){
+        m.makeAllUsersActive();
+        v.refreshTable();
     }
 
-    
 }
